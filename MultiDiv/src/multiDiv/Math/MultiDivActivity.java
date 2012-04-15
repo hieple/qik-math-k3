34f535e9269df185ad7df4882721ebf39dae03a7
Hiep Le <hiep.h.le@gmail.com>
2012-04-14 23:57:33 -0700

    layout

--- MultiDiv/src/multiDiv/Math/MultiDivActivity.java
+++ MultiDiv/src/multiDiv/Math/MultiDivActivity.java
@@ -3,9 +3,8 @@ package multiDiv.Math;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MultiDivActivity extends Activity {
 	@Override
@@ -13,36 +12,34 @@ public class MultiDivActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-	}
+		final Button button1 = (Button) findViewById(R.id.mRpt);
+		button1.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent ite1 = new Intent(MultiDivActivity.this,
+						ReportView.class);
+				ite1.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				startActivity(ite1);
+			}
+		});
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.menu_main, menu);
-		return true;
-	}
+		final Button button2 = (Button) findViewById(R.id.mCfg);
+		button2.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent ite2 = new Intent(MultiDivActivity.this,
+						ConfigMath.class);
+				ite2.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				startActivity(ite2);
+			}
+		});
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case R.id.mreport:
-			Intent ite1 = new Intent(MultiDivActivity.this, ReportView.class);
-			ite1.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-			startActivity(ite1);
-			return true;
-		case R.id.mconfig:
-			Intent ite2 = new Intent(MultiDivActivity.this, ConfigMath.class);
-			ite2.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-			startActivity(ite2);
-			return true;
-		case R.id.mpractice:
-			Intent ite3 = new Intent(MultiDivActivity.this, MainWork.class);
-			ite3.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-			startActivity(ite3);
-			return true;		
-		default:
-			return true;
-		}
+		final Button button3 = (Button) findViewById(R.id.mPrc);
+		button3.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent ite3 = new Intent(MultiDivActivity.this, MainWork.class);
+				ite3.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				startActivity(ite3);
+			}
+		});
 	}
 
 }
