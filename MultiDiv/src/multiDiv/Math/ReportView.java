dc5da19fe8b53dd7fff0d2aee6cabe4fcc2104c6
Hiep Le <hiep.h.le@gmail.com>
2012-05-01 03:54:29 -0700

    result detail landscape

--- MultiDiv/src/multiDiv/Math/ReportView.java
+++ MultiDiv/src/multiDiv/Math/ReportView.java
@@ -1,23 +1,16 @@
 package multiDiv.Math;
 
 import java.util.ArrayList;
-import multiDiv.Math.Comp.TextViewMa;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewGroup.LayoutParams;
 import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.GridView;
 import android.widget.ListView;
-import android.widget.TableLayout;
-import android.widget.TableRow;
 import android.widget.TextView;
 
 public class ReportView extends Activity {
@@ -73,59 +66,9 @@ public class ReportView extends Activity {
 
 	void LoadReport() {
 
-		// GridView Gv = (GridView) findViewById(R.id.gDetail);
-		// Gv.setAdapter(new ResultAdapter(this));
-
 		ListView Lv = (ListView) findViewById(R.id.vDetail);
 		Lv.setAdapter(new ResultAdapter(this));
-		/*
-		 * 
-		 * ArrayList<String> s = ConfigGame.GetResult(this);
-		 * 
-		 * TableLayout tl = (TableLayout) findViewById(R.id.gDetail);
-		 * 
-		 * // Do not make header when no data if (s.size() == 1) { String[] sa =
-		 * s.get(0).split("\\|"); if (sa.length > 1) SetTableHeader(tl); } else
-		 * SetTableHeader(tl);
-		 * 
-		 * for (int c = 0; c < s.size(); c++) { TableRow tr = new
-		 * TableRow(this);
-		 * 
-		 * String[] sa = s.get(c).split("\\|"); for (int e = 0; e < sa.length;
-		 * e++) {
-		 * 
-		 * TextViewMa labelTV = new TextViewMa(this); labelTV.setText(sa[e]);
-		 * 
-		 * labelTV.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20);
-		 * labelTV.setPadding(10, 10, 10, 10);
-		 * labelTV.setGravity(Gravity.RIGHT);
-		 * 
-		 * tr.addView(labelTV); }
-		 * 
-		 * tl.addView(tr, new TableLayout.LayoutParams(
-		 * LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)); }
-		 */
 	}
-
-	void SetTableHeader(TableLayout tl) {
-		String[] Header = { "Date", "Time", "Elapsed Time", "Correct", "Wrong",
-				"Average", "Mode" };
-		TableRow tr = new TableRow(this);
-
-		for (int i = 0; i < Header.length; i++) {
-
-			TextViewMa Title = new TextViewMa(this);
-			Title.setText(Header[i]);
-			Title.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20);
-			Title.setPadding(5, 5, 5, 5);
-			Title.setTextColor(Color.BLUE);
-			tr.addView(Title);
-		}
-		tl.addView(tr, new TableLayout.LayoutParams(LayoutParams.MATCH_PARENT,
-				LayoutParams.WRAP_CONTENT));
-
-	}
-
 }
 
 class ResultAdapter extends BaseAdapter {
@@ -167,7 +110,6 @@ class ResultAdapter extends BaseAdapter {
 			String[] s = _content.get(position).split("\\|");
 			tv0.setText(s[0]);
 			tv1.setText(s[1]);
-
 			tv2.setText(s[2]);
 			tv3.setText(s[3]);
 			tv4.setText(s[4]);
